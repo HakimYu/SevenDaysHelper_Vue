3afcf9644a2c5cb6acb1bfa66625c62e735a842c
HakimYu <61399973+HakimYu@users.noreply.github.com>
2022-05-20 03:39:01 +0000

    Deploying to pages from @ HakimYu/SevenDaysHelper_Vue@225a774a7a43e5a0240d1a24c8e0f7455bfaee39 🚀

--- vetur.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    settings: {
-        "vetur.useWorkspaceDependencies": true,
-        "vetur.experimental.templateInterpolationService": true,
-        "vetur.ignoreProjectWarning": true
-    },
-}
